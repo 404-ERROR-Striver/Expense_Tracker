5c3326e6aaa26b54a77d292c5a1796e07573392b
404-ERROR-Striver <ansarimntsh07@gmail.com>
2025-11-16 16:17:36 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+ app.get("/",(req,res)=>{
+        res.send("it is working");
+    });
+
+//  console.log("my port:",process.env.port)   
+
+
+app.listen(PORT,()=>{
+    console.log("server is up  and running on PORT:",PORT);
+   
+});
