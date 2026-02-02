4aa9e5c32907d3f6c86e8102e3b3f267e98e6a79
404-ERROR-Striver <ansarimntsh07@gmail.com>
2026-02-02 23:48:09 +0530

    Clarify SQL connection code in db.js
    Updated  code  purpose of the SQL connection.

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,6 @@ import {neon} from "@neondatabase/serverless";
 
 import "dotenv/config";
 
-//create a SQL connection using our DB URL 
+//create a SQL connection using our DB URL  basic // basic sql connection using  our db url 
 
 export const sql = neon(process.env.DATABASE_URL);
